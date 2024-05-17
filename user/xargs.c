65ceedb70a2fdb3f63df3b3a3b1ee4058f97343b
HOLIC0105 <3485995896@qq.com>
2024-05-17 16:28:13 +0800

    实现xargs操作,完成lab1所有grade内容

---
+++ user/xargs.c
@@ -0,0 +1,49 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/param.h"
+
+#define MAXLINE       1024  // 每一行的最大字节数
+
+int readline(int fd, char *buf) {
+    int len = 0;
+    char *p = buf;
+    while(read(fd, p, 1) > 0) {
+        len ++;
+        if(*p == '\n') break;
+        p ++;
+    }
+    return len;        
+}
+
+char whitespace[] = " \t\r\n\v";
+
+int
+main(int argc, char *argv[])
+{   
+    char buf[MAXLINE];
+    char * execargv[MAXARG];
+    int execargc = argc - 1;
+    for(int i = 1; i < argc; i ++)
+        execargv[i - 1] = argv[i];
+    int len;
+    while((len = readline(0, buf)) > 0) {
+        buf[len] = 0;
+        execargc = argc - 1;
+        for(int i = 0; i < len; i ++) {
+            while(i != len && strchr(whitespace, buf[i])) i ++;
+            if(i != len) {
+                execargv[execargc ++] = buf + i;
+                while(i != len && !strchr(whitespace, buf[i])) i ++;
+                buf[i] = 0;
+            }
+        }
+        execargv[execargc] = 0;
+        if(fork() == 0) {
+            exec(execargv[0], execargv);
+        } else wait(0);
+    }
+    exit(0);
+}
+
+
