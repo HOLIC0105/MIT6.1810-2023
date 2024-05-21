8486d0056d62c4388db7fd7b99866bef0cac9316
HOLIC0105 <3485995896@qq.com>
2024-05-21 10:47:23 +0800

    find指令现在可以支持正则表达式了

--- user/find.c
+++ user/find.c
@@ -4,6 +4,8 @@
 #include "kernel/fs.h"
 #include "kernel/fcntl.h"
 
+int match(const char*, const char*);
+
 void
 find(const char *path, const char *filename) {
     char buf[512], *p;
@@ -39,7 +41,7 @@ find(const char *path, const char *filename) {
         {
         case T_DEVICE:
         case T_FILE:
-            if(strcmp(de.name, filename) == 0) {
+            if(match(filename, de.name)) {
                 fprintf(1, "%s\n", buf);
             }
             break;
@@ -62,6 +64,46 @@ main(int argc, char const *argv[])
     }
     find(argv[1], argv[2]);
     exit(0);
+}
+
+
+int matchhere(const char*, const char*);
+int matchstar(int, const char*, const char*);
+
+int
+match(const char *re, const char *text)
+{
+  if(re[0] == '^')
+    return matchhere(re+1, text);
+  do{  // must look at empty string
+    if(matchhere(re, text))
+      return 1;
+  }while(*text++ != '\0');
+  return 0;
+}
+
+// matchhere: search for re at beginning of text
+int matchhere(const char *re, const char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
+}
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, const char *re, const char *text)
+{
+  do{  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
 }
 
 
